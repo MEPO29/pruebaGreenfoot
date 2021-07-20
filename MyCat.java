40931cf14825a6b0dfacc0b428d08e94fc7c3633
MEPO29 <mepopuente29@gmail.com>
2021-07-20 08:11:39 -0600

    Primer commit

---
+++ MyCat.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * MyCat is your own cat. Get it to do things by writing code in its act method.
+ * 
+ * @Mario Puente (your name) 
+ * @version 1 (a version number or a date)
+ */
+public class MyCat extends Cat
+{
+    /**
+     * Act - do whatever the MyCat wants to do.
+     */
+    public void act()
+    {
+        eat();
+        dance();
+        
+    }    
+}
